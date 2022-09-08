de00f281d202612531d37e824490bc0776574ee7
Semyon Levin <231188+remal@users.noreply.github.com>
2022-09-07 20:36:44 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -51,6 +51,7 @@ async function run(): Promise<void> {
 
     } catch (error) {
         core.setFailed(error instanceof Error ? error : (error as object).toString())
+        throw error
     }
 }
 
